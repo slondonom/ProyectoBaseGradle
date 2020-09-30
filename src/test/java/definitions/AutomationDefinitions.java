cc80cfa44d42f0f525c1eabe3e09ca8bf6c96cc7
slondonom <sebaslondono2@utp.edu.co>
2020-09-30 10:01:39 -0500

    add file .gitignore

---
+++ src/test/java/definitions/AutomationDefinitions.java
@@ -0,0 +1,88 @@
+package definitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import net.thucydides.core.annotations.Steps;
+import steps.AutomationSteps;
+
+import java.util.List;
+
+public class AutomationDefinitions {
+    @Steps
+    AutomationSteps automationSteps;
+    @Given("^The user is on Homepage$")
+    public void theUserIsOnHomepage() {
+        automationSteps.openHomePage();
+    }
+
+    @Then("^the user clicks on Sign in$")
+    public void theUserClicksOnSignIn() {
+        automationSteps.SignIn();
+
+    }
+
+    @Then("^user enters email$")
+    public void userEntersEmail(List<String> parameters) {
+        automationSteps.userEmail(parameters);
+
+    }
+
+    @Then("^User Clicks create an account$")
+    public void userClicksCreateAnAccount() {
+        automationSteps.submitCreateAccount();
+    }
+
+    @Then("^User fills sign up form$")
+    public void userFillsSignUpForm(List<String> parameters) {
+        automationSteps.fillsForms(parameters);
+    }
+
+    @Then("^user clicks register$")
+    public void userClicksRegister() {
+        automationSteps.userClicksRegister();
+    }
+
+    @Then("^the user is taken my account page$")
+    public void theUserIsTakenMyAccountPage(List<String> parameters) {
+        automationSteps.verifyMyAccount(parameters);
+    }
+
+
+
+    @Given("^the user has a valid account$")
+    public void theUserHasAValidAccount() {
+        automationSteps.openHomePage();
+    }
+
+    @Then("^the clicks on sign in$")
+    public void theClicksOnSignIn() {
+        automationSteps.SignIn();
+    }
+
+    @Then("^enters email$")
+    public void entersEmail(List<String> parameters) {
+        automationSteps.emailRegister(parameters);
+    }
+
+    @Then("^enters password$")
+    public void entersPassword(List<String> parameters) {
+        automationSteps.enterPassword(parameters);
+
+    }
+
+    @Then("^clicks sign in$")
+    public void clicksSignIn() {
+        automationSteps.clickSingInRegister();
+
+    }
+
+    @Then("^the user is successfully logged in$")
+    public void theUserIsSuccessfullyLoggedIn(List<String> parameters) {
+        automationSteps.verifyMyAccount(parameters);
+
+    }
+
+
+
+
+}
---
+++ src/test/resources/features/automation.feature
@@ -0,0 +1,34 @@
+@Account
+Feature: User Creates an account
+
+  @RegisterAccount
+  Scenario Outline: User can create an account
+    Given The user is on Homepage
+    Then the user clicks on Sign in
+    And user enters email
+      | <Email> |
+    And User Clicks create an account
+    And User fills sign up form
+      | <FirstName> | <LastName> | <Email> | <Pass> | <Company> | <AddressOne> | <AddressTwo> | <City> | <PostalCode> | <OtherInfo> | <HomePhone> | <MobilePhone> | <Alias> |
+    And user clicks register
+    Then the user is taken my account page
+      | <ConfirmAccount> |
+
+    Examples:
+      | FirstName | LastName | Email                               | Pass      | Company  | AddressOne  | AddressTwo  | City     | PostalCode | OtherInfo | HomePhone | MobilePhone | Alias          | ConfirmAccount |
+      | Sebastian | Londono  | sebastianL@gmail.com | R3alPage. | RealPage | Cr87a#32-81 | Cr87a#32-81 | Medellin | 33206      | Hiring    | 7748954   | 3005647895  | TestAutomation | MY ACCOUNT     |
+
+  @LogInAccount
+  Scenario Outline: user can log in account
+    Given the user has a valid account
+    Then the clicks on sign in
+    And enters email
+      | <Email> |
+    And enters password
+      | <Pass> |
+    And clicks sign in
+    Then the user is successfully logged in
+      | <ConfirmAccount> |
+    Examples:
+      | Email                   | Pass      | ConfirmAccount                                                                            |
+      | sloondonom@gmail.com | R3alPage. | MY ACCOUNT  |
